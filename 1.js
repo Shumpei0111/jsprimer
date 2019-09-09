db0d65959aab8d7866fe2640c8872b6f8757341f
Otsubo <ohtsubo@qzc.co.jp>
2019-09-09 16:19:39 +0900

    null

--- 1.js
+++ 1.js
@@ -59,3 +59,103 @@ function constAndLet(){
   // まずconstで定義できないか検討し、出来ない場合はletで書いていく
 }
 constAndLet();
+
+
+function checkNumber() {
+// 整数リテラル
+  // 10進数
+    // ただし、複数の数字を組み合わせた際に、先頭を0から始めると8進数として扱われる場合がある
+    console.log(3, 1);
+    console.log(4, 10);
+
+    // 非推奨な8進数の書き方
+    // use strictモードだとエラー
+    console.log(5, 0644); // 420
+    console.log(6, 0777); // 511
+
+  // 2進数
+  // bはbinaryの意味。ビットを表現するのに利用
+  console.log(7, 0b0); // 0
+  console.log(8, 0b1010); // 10
+
+  // 8進数
+  // oはoctalの意味。ファイルのパーミッションを表現するのに利用
+  console.log(9, 0o644); // 420
+  console.log(10, 0o777); // 511
+
+  // 16進数
+  // xはhexの意味。文字のコードポイントやRGB値を表現するのに利用
+  console.log(11, 0xFF); // 255
+  console.log(12, 0x30A2); // 12450
+
+
+// 不動小数点リテラル
+  // 0から始まる場合は省略可能だが非推奨
+  console.log(13, .123); // 0.123
+
+  // eは指数を意味する。
+  // 2e8は2x10の8乗
+  console.log(14, 2e8); // 200000000
+}
+checkNumber();
+
+
+function checkString(){
+// 文字列リテラル
+  // ダブルクオートとシングルクオートは同じ文字列扱いとして評価される
+
+  // バックスラッシュ
+  console.log(15, "8 o'clock");  // "8 o'clock"
+  console.log(16, '8 o\'clock'); // "8 o'clock"
+
+  // 改行
+  console.log(17, "改行前\n改行後");
+  
+// ES2015(ES6)での機能
+  // 改行
+  console.log(18, `改行前
+  改行後`);
+
+  // テンプレート機能
+  // バッククオートで囲う
+  const str = "string";
+  console.log(19, `this is ${str}`); // "this is string"
+
+  const s = "`str`";
+  console.log(19.1, `this is ${s}`); // "this is `str`"
+  
+  // リテラルを含める場合、エスケープを使う
+  console.log(20, `this is \`str\``); // "this is `str`"
+
+}
+checkString();
+
+
+function checkNull(){
+// null
+  // null = 値がない
+  // 未定義の変数を参照するとエラー
+
+  // console.log(21, foo); <= Uncaught ReferenceError: foo is not defined
+  const foo = null;
+  console.log(21.1, foo); // null
+
+  // nullにオブジェクトの値を代入しようとしてもエラーになる
+  // foo.hoge = "aaa";
+  // console.log(21.2, foo); <= Uncaught TypeError: Cannot set property 'hoge' of null
+
+  // constで定義しているので再代入はもちろん不可
+  // foo = {};
+  // console.log(21.1-1, foo); <= Uncaught TypeError: Assignment to constant variable.
+
+  // letだと再代入可能
+  let f = null;
+  console.log(21.3, f); // null
+
+  f = {};
+  console.log(21.4, f); // {}
+  
+  f.hoge = "bbb";
+  console.log(21.5, f); // {hoge: "bbb"}
+}
+checkNull();
