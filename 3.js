0f6e95079dee0e19fc6f26cf53549238900fd8b4
Otsubo <ohtsubo@qzc.co.jp>
2019-10-29 17:50:39 +0900

    条件分岐とループ

--- 3.js
+++ 3.js
@@ -184,4 +184,362 @@ function checkIf(){
 checkIf();
 
 
-// 
+// Switch文
+
+function checkSwitch() {
+  switch("式") {
+    case "label1":
+      // 式の評価結果がlabel1と一致する場合に実行
+      break;
+
+    case "label2":
+      // 式の評価結果がlabel2と一致する場合に実行
+      break;
+
+    default:
+      // どのcaseにも該当しない場合の処理
+      break;
+  }
+
+  const ver = "ES6";
+  switch(ver) {
+    case "ES5":
+      console.log("ECMAscript 5");
+      break;
+    case "ES6":
+      console.log("ECMAscript 2015");
+      break;
+    case "ES7":
+      console.log("ECMAscript 2016");
+      break;
+    default:
+      console.log("no js");
+      break;
+  } // ECMAscript 2015
+}
+checkSwitch();
+
+
+// break文
+// switch文のcase節では基本的にbreak;を使いswitch文を抜けるようにする
+// break;は省略可能だが、省略した場合、後ろに続くcase節が条件に関係なく実行されてしまう
+
+function checkBreak() {
+  const ver = "ES6";
+  switch(ver) {
+    case "ES5":
+      console.log(3,"ECMAscript 5");
+    case "ES6":
+      console.log(3,"ECMAscript 2015"); // ES6にbreak;がないからこの下から無条件に実行される
+    case "ES7":
+      console.log(3,"ECMAscript 2016"); // break;がないから無条件に実行される
+    default:
+      console.log(3,"no js"); // break;がないから無条件に実行される
+  }
+
+
+  /*************************************************************************************/
+  /*     break;はif文の代わりに使わないで関数と組み合わせて条件に対する値を返すときに使う     */
+  /*************************************************************************************/
+  function getESName(ver) {
+    switch(ver) {
+      case "ES5":
+        return "ECMAscript 5";
+      case "ES6":
+        return "ECMAscript 2015";
+      case "ES7":
+        return "ECMAscript 2016";
+      default:
+        return "no js";
+    }
+  }
+  let g = getESName("ES5");
+  console.log(4, g);
+}
+checkBreak();
+
+
+
+/****************************************** ループと反復処理 ********************************************/
+
+// fizz buzz
+function fzbz() {
+  for (let i = 0; i <= 30; i++) {
+    if(i % 15 === 0) {
+      console.log("fizz buzz");
+    } else if(i % 5 === 0) {
+      console.log("buzz");
+    } else if(i % 3 === 0) {
+      console.log("fizz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+fzbz();
+
+
+// while文
+// while文は女見識がtrueならば反復処理を行う
+function checkWhile(){
+  while(true) {
+    // 実行する文;
+    break;
+  }
+
+  let x = 0;
+  console.log(`ループ開始前の値: ${x}`);
+  while(x < 10) {
+    console.log(x);
+    x += 1;
+  }
+  console.log(`ループ終了後のxの値: ${x}`);
+  
+  // breakや終了条件を書かないと終わらないので無限ループする
+  // 安易にwhileを使わずに他の手段をまず考える
+}
+checkWhile();
+
+
+// do-while文
+// while文と実行順序が異なる
+//
+// 1. 実行する文を実行
+// 2. 条件式の評価結果がtrueなら次ステップ、falseなら終了
+// 3. ステップ1に戻る
+//
+// そのため、1回は必ず実行する
+function doWhile(){
+  do {
+    // 実行する文;
+    break;
+  } while ("条件式");
+}
+doWhile();
+// ただこちらも他の手段を考える
+
+
+// for文
+// for文は繰り返す範囲を指定した反復処理を書くことが出来る
+function checkFor() {
+
+  let total = 0;
+  for (let i = 0; i < 10; i++) {
+    total += i + 1;
+  }
+  console.log(total);
+
+  // 任意の数値が入った配列を受け取り、その合計値を返す
+  // num配列に含まれている要素を先頭から順番に変数totalに加算し合計値を計算している
+  //
+  // ※配列は順番が保証されている
+  function sum(num) {
+    let total = 0;
+    for (let i = 0; i < num.length; i++) {
+      total += num[i];
+    }
+    return total;
+  }
+  console.log(sum([1,2,3,4,5]));
+}
+checkFor();
+
+
+
+// 配列のforEachメソッド
+//
+// JSでは関数がファーストクラスであるため、その場で作った無名関数を引数として渡すことが出来る
+//
+// 引数として渡される関数のことを ** コールバック関数 ** と呼ぶ
+// コールバック関数を引数として受け取る関数やメソッドを高階関数とよぶ
+function forEachCheck() {
+  const arr = [{a:1, b:100}, 2, 3];
+  arr.forEach(currentVal => {
+    // ループごとに実行する処理
+    console.log(currentVal);
+    arr.push(2);
+  });
+  console.log(arr);
+  // forEachメソッドのコールバック関数には、配列の要素が先頭から順番に渡されて実行される
+  // 上記の例では、currentValには1から3の値が順に渡される
+
+
+  function sum(num) {
+    let total = 0;
+    num.forEach(n => {
+      total += n;
+    });
+    return console.log(total);
+  }
+  sum([1,2,3,4,5]);
+
+  // forEachはfor文の条件式に相当するものはなく、必ず配列のすべての要素を反復処理する
+  // 変数i といった一時的な値を定義する必要がないので、シンプルに反復処理が書ける
+}
+forEachCheck();
+
+
+
+// break文
+// while, do-while, forの中で使う
+// 処理中のループを抜けて次の文へ制御を移す
+function breakCheck() {
+  while(true) {
+    break; // *1へ
+  }
+  // *1 次の文
+
+  // switch文で出てきたものと同様で、処理中のループ文を終了できる
+  // 次のコードでは、配列の要素に一つでも偶数を含んでいるかを判定している
+  const nums = [1, 5, 10, 15, 20];
+
+  let isEvenIncluded = false;
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    if ( num % 2 === 0) {
+      isEvenIncluded = true;
+      break;
+    }
+  }
+  console.log(isEvenIncluded); // true
+
+
+
+  // たいていこのような処理は使い回せるように関数として実装する
+  
+  // 引数のnが偶数ならtrueを返す
+  function isEven(n) {
+    return n % 2 === 0;
+  }
+
+  // 引数のnumに偶数が含まれているならtrueを返す
+  function isEvenInc(num) {
+    let isEvenInc = false;
+    for (let i = 0; i < num.length; i++) {
+      const n = num[i];
+      if(isEven(n)) {
+        isEvenInc = true;
+        break;
+        // 1行で return ture; でも可
+        // そうすることで、ひとつでも偶数があった場合、その瞬間にループを抜けられる
+      }
+    }
+    return isEvenInc; // 偶数が含まれていなかったら1行目のfalseが返ってくる
+    // 上でreturn trueで返したならreturn falseにする
+  }
+  const arr = [1, 5, 10, 15, 20];
+  console.log(20, isEvenInc(arr)); // true
+}
+breakCheck();
+
+
+// 配列のsomeメソッド
+// someメソッドは、配列の各要素をテストする処理をコールバック関数として受け取る
+// コールバック関数が一度でもtrueを返した時点で反復処理を終了し、trueを返す
+function arrSome() {
+  const arr = [1, 2, 3, 4, 5];
+  const isPassed = arr.some(currentVal => {
+    // テストをパスするtrue,
+    // そうでないならfalse
+  })
+
+  // someメソッドを使い、配列に偶数が含まれているか
+  // 受け取った値が偶数であるかをテストするコールバック関数としてisEven関数を渡す
+  function isEven(num) {
+    return num % 2 === 0;
+  }
+
+  const nums = [1, 3, 4, 5];
+  console.log(nums.some(isEven)); // true
+}
+arrSome();
+
+
+// continue文
+// continue文は現在の反復処理を終了し、次の反復処理を行う
+// while, do-while, forの中で使用可能
+
+// while文の処理中でcontinueが実行されると、現在の反復処理はその時点で終了される
+// そして、次の反復処理で条件式を評価するところからループが再開される
+
+function checkContinue() {
+  // while("条件式") {
+  //   // 実行される処理
+  //   continue; // 条件式へ
+  //   // これ以降の行は実行されない
+  // }
+
+  // 配列の中から偶数を集め、新しい配列を作り変えしています
+  // 偶数でない場合、処理中のfor文をスキップする
+
+  // numが偶数ならtrueを返す
+  function isEven(num) {
+    return num % 2 === 0;
+  }
+
+  // numsに含まれている偶数だけ取り出す
+  function filterEven(nums) {
+    const res = [];
+    for (let i = 0; i < nums.length; i++) {
+      const num = nums[i];
+
+      // 偶数ではないなら、次のループへ
+      if (!isEven(num)) {
+        continue;
+      }
+
+      // 偶数をresに追加
+      res.push(num);
+    }
+    return res;
+  }
+
+  const arr = [1, 5, 10, 15, 20];
+  console.log(filterEven(arr));
+
+
+  // continueを使わずに偶数ならresへ追加する書き方も可能
+  // だけど条件が複雑になるとすぐにネストが深くなるから読みにくくなりがち
+  function notUseContinue() {
+    if (isEven(num)) {
+      res.push(num);
+    }
+  }
+
+  //うるう年判定のようにネストしない書き方でがんばる
+  // const y = new Date().getFullYear();
+  const y = 200;
+  if( y % 400 === 0 ) {
+    console.log(`${y}うるう`);
+  } else if (y % 100 === 0) {
+    console.log(`${y}うるわない`);
+  } else if (y % 4 === 0){
+    console.log(`${y}うるう`)
+  } else {
+    console.log(`${y}うるわない`);
+  }
+}
+checkContinue();
+
+
+// filterメソッド
+// 配列から特定の値だけを集めた新しい配列を作るにはfilterメソッドを使う
+//
+// filterメソッドには、配列の各要素をテストする処理をコールバック関数として渡す
+// コールバック関数がtrueを返した要素のみを集めた新しい配列を返す
+function arrFilter() {
+  const arr = [1, 2, 3, 4, 5];
+  const filteredArr = arr.filter((currentVal, ind, arr) => {
+    // テストをパスするならtrue, そうでないならfalse
+  });
+
+  // 先のcontinueを使った値の絞り込みはfilterメソッドを使うとより完結に書ける
+  // 次のコードでは、filterメソッドを使い偶数だけに絞り込んでいる
+  function isEven(num) {
+    return num % 2 === 0;
+  }
+
+  const ar = [1, 2, 3, 4, 5];
+  console.log(22,ar.filter(isEven));
+}
+arrFilter();
